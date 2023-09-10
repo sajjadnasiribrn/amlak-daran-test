c64b804dc296f3f40965a07bfcb44c9840372a87
sajjadnasiribrn <65922402+sajjadnasiribrn@users.noreply.github.com>
2023-09-10 23:38:54 +0430

    make model and migration

---
+++ src/database/migrations/2023_09_10_173429_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->timestamp('date_of_birth');
+
+            $table->string('phone_number', 14);
+
+            $table->string('email', 255);
+
+            $table->string('bank_account_number', 255);
+
+            $table->primary(['first_name', 'last_name', 'date_of_birth']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
